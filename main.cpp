072017539d26d3935d90ac4f71c282bb304311f6
ItsAlanK <alankinsella1993@gmail.com>
2023-10-03 21:21:14 +0100

    Add comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "raylib.h"
 
 int main(){
+    // Set window size
     const int windowWidth{500};
     const int windowHeight{500};
     
     InitWindow(windowWidth,windowHeight,"My Window");
 
+    // Set FPS
     SetTargetFPS(60);
 
+    // Main game loop
 	while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE); 
