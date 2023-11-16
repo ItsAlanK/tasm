7a70a94a803f6f700030bccaca56fb3c35edbce6
ItsAlanK <alankinsella1993@gmail.com>
2023-11-16 20:07:25 +0000

    Declare basic object class with draw function. Test drawing of object to screen

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include "raylib.h"
 
+
+// Basic rectangle object class player char and obstacles will build off
+// I'm going to move things to their own files once they're tested as working here to be safe
+class Object {
+    private:
+    int posX;
+    int posY;
+    int width;
+    int height;
+    Color color;
+
+    public:
+    Object(int x, int y, int width, int height, Color color)
+    :posX(x), posY(y), width(width), height(height), color(color) {}
+
+    void Draw() const{
+        DrawRectangle(posX, posY, width, height, color);
+    }
+};
+
 int main(){
     // Set window size
     const int windowWidth{750};
     const int windowHeight{750};
     
     InitWindow(windowWidth,windowHeight,"TASM");
+
+    Object basicObject(windowWidth/2 - 50, windowHeight/2 - 100, 100, 200, RED);
 
     // Set FPS
     SetTargetFPS(60);
@@ -14,6 +36,7 @@ int main(){
 	while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(WHITE);
+        basicObject.Draw();
         EndDrawing();
     }
 
