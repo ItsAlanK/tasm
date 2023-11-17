a43cf35e1b613e4ee06fa842a97610b2058e14d3
ItsAlanK <alankinsella1993@gmail.com>
2023-11-17 20:33:41 +0000

    Fix linting in cpp files for readability

--- src/components/base_object.cpp
+++ src/components/base_object.cpp
@@ -1,3 +1,6 @@
 #include "base_object.h"
 
-Object::Object(int x, int y, Color color):posX(x), posY(y), color(color) {}
+Object::Object(int x, int y, Color color)
+    :posX(x),
+    posY(y),
+    color(color){}
--- src/components/player.cpp
+++ src/components/player.cpp
@@ -1,6 +1,9 @@
 #include "player.h"
 
-Player::Player(int x, int y, int width, int height, Color color): Object(x, y, color), width(width), height(height) {}
+Player::Player(int x, int y, int width, int height, Color color)
+    :Object(x, y, color),
+    width(width),
+    height(height){}
 
 // Draw object to screen
 void Player::Draw(){
